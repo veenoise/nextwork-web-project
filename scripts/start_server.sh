ab531f5b22f22842e891818a42148e9e55d8fc48
William Chua <william.eduard123@gmail.com>
2025-09-04 17:12:42 +0800

    Reset repo

--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo systemctl start tomcat.service
-sudo systemctl enable tomcat.service
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp="$(pgrep httpd)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
-fi
-isExistApp="$(pgrep tomcat)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
-fi
